8b93c218e1bb5d6c384b5e96b9714a7194dcdd2c
ned777 <nmdcnn@gmail.com>
2026-04-19 02:13:20 -0700

    added import

--- sorter.py
+++ sorter.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 
+"""Phone media sorter. Polls landing folder, sorts to year-organized destinations."""
+import logging, os, shutil, sys, time
+from datetime import datetime
+from pathlib import Path
+from PIL import Image, ExifTags
+from pymediainfo import MediaInfo
+
